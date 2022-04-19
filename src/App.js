d091142fb6e1f86f205af1f61dc08c88f37781a1
truong score <truongscore@gmail.com>
2022-04-19 09:50:53 +0700

    Update code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Truong Score was visited
+          Truong Score was visited. Push code already.
         </p>
         <a
           className="App-link"
